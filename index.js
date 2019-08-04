0328dbe488fa4464ddee2770d1f6b4cdbdafbb66
BannerBomb <BannerBomb55@gmail.com>
2019-08-03 20:39:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	ms: require('./ms'),
 	parseMilliseconds: require('./parse-ms'),
 	humanize: require('./pretty-ms'),
+	humanise: require('./pretty-ms'),
 	prettyMilliseconds: require('./pretty-ms')
 }
