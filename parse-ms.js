9dc9aac43a8d4107747dcb8d490668de82804b1f
BannerBomb <BannerBomb55@gmail.com>
2019-08-05 01:08:59 -0400

    Update parse-ms.js

--- parse-ms.js
+++ parse-ms.js
@@ -1,16 +1,20 @@
 'use strict';
-const now = new Date();
-const days_in_month = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate(); // was + 1
-const weeks_in_month = days_in_month / 7;
-
+const leap_year = (year) => ((!(year % 4) && !!(year % 100)) || !(year % 400)) ? 366 : 365;
 module.exports = (milliseconds) => {
 	if (typeof milliseconds !== 'number') {
 		throw new TypeError('Expected a number');
 	}
+	
+	const ms_date = new Date(milliseconds);
+	const ms_year = ms_date.getFullYear();
+	const days_in_month = new Date(ms_year, ms_date.getMonth()+1, 0).getDate();
+	const weeks_in_month = days_in_month / 7;
+	const days_in_year = leap_year(ms_year);
+	milliseconds = Date.now() - milliseconds;
 
 	const roundTowardsZero = milliseconds > 0 ? Math.floor : Math.ceil;
 	return {
-      		years: roundTowardsZero(milliseconds / ((1000 * 60 * 60 * 24 * days_in_month) * 12)), // 31557600000 // 31540000000 // 31536000000
+      		years: roundTowardsZero(milliseconds / (1000 * 60 * 60 * 24 * days_in_year)), // 31557600000 // 31540000000 // 31536000000
 		months: roundTowardsZero(milliseconds / (1000 * 60 * 60 * 24 * days_in_month)) % 12,//) % 12, //2419200000 // 2628000000
 		weeks: roundTowardsZero(milliseconds / (1000 * 60 * 60 * 24 * 7)) % Math.ceil(weeks_in_month),
 		days: roundTowardsZero(milliseconds / (1000 * 60 * 60 * 24)-1) % 7,
