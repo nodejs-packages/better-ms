e1c4b0ffcbf0c05438373ec63b545e9ce4c905bb
BannerBomb <BannerBomb55@gmail.com>
2019-11-19 13:37:35 -0500

    Update and rename index.js to index.js.bak

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	ms: require('./ms'),
-	parseMilliseconds: require('./parse-ms'),
-	humanize: require('./pretty-ms'),
-	humanise: require('./pretty-ms'),
-	prettyMilliseconds: require('./pretty-ms')
-}
