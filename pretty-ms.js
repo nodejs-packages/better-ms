10e44c99b053f091206d220c04a46b611f0e4c3e
BannerBomb <BannerBomb55@gmail.com>
2020-07-17 12:05:51 -0400

    Update pretty-ms.js

--- pretty-ms.js
+++ pretty-ms.js
@@ -1,15 +1,27 @@
 'use strict';
+
+/* Version 7.0.0 */
+
 const parseMilliseconds = require('./parse-ms');
 
 const now = new Date();
 const days_in_month = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
 const weeks_in_month = days_in_month / 7;
 
-const pluralize = (word, count) => count === 1 ? word : word + 's';
+const pluralize = (word, count) => count === 1 ? word : `${word}s`;
+
+const SECOND_ROUNDING_EPSILON = 0.0000001;
 
 module.exports = (milliseconds, options = {}) => {
 	if (!Number.isFinite(milliseconds)) {
 		throw new TypeError('Expected a finite number');
+	}
+
+	if (options.colonNotation) {
+		options.compact = false;
+		options.formatSubMilliseconds = false;
+		options.separateMilliseconds = false;
+		options.verbose = false;
 	}
 
 	if (options.compact) {
@@ -19,24 +31,33 @@ module.exports = (milliseconds, options = {}) => {
 
 	const result = [];
 
+	const floorDecimals = (value, decimalDigits) => {
+		const flooredInterimValue = Math.floor((value * (10 ** decimalDigits)) + SECOND_ROUNDING_EPSILON);
+		const flooredValue = Math.round(flooredInterimValue) / (10 ** decimalDigits);
+		return flooredValue.toFixed(decimalDigits);
+	};
+
 	const add = (value, long, short, valueString) => {
-		if (value === 0) {
+		if ((result.length === 0 || !options.colonNotation) && value === 0 && !(options.colonNotation && short === 'm')) {
 			return;
 		}
 
-		const postfix = options.verbose ? ' ' + pluralize(long, value) : short;
-
-		result.push((valueString || value) + postfix);
-	};
-
-	const secondsDecimalDigits = typeof options.secondsDecimalDigits === 'number' ? options.secondsDecimalDigits : 1;
-
-	if (secondsDecimalDigits < 1) {
-		const difference = 1000 - (milliseconds % 1000);
-		if (difference < 500) {
-			milliseconds += difference;
+		valueString = (valueString || value || '0').toString();
+		let prefix;
+		let suffix;
+		if (options.colonNotation) {
+			prefix = result.length > 0 ? ':' : '';
+			suffix = '';
+			const wholeDigits = valueString.includes('.') ? valueString.split('.')[0].length : valueString.length;
+			const minLength = result.length > 0 ? 2 : 1;
+			valueString = '0'.repeat(Math.max(0, minLength - wholeDigits)) + valueString;
+		} else {
+			prefix = '';
+			suffix = options.verbose ? ' ' + pluralize(long, value) : short;
 		}
-	}
+
+		result.push(prefix + valueString + suffix);
+	};
 
 	const parsed = parseMilliseconds(milliseconds);
   	add(parsed.years, 'year', 'y');
@@ -56,17 +77,21 @@ module.exports = (milliseconds, options = {}) => {
 			const millisecondsAndBelow = parsed.milliseconds + (parsed.microseconds / 1000) + (parsed.nanoseconds / 1e6);
 
 			const millisecondsDecimalDigits = typeof options.millisecondsDecimalDigits === 'number' ? options.millisecondsDecimalDigits : 0;
+			
+			const roundedMiliseconds = millisecondsAndBelow >= 1 ?
+				Math.round(millisecondsAndBelow) :
+				Math.ceil(millisecondsAndBelow);
 
-			const millisecondsString = millisecondsDecimalDigits ? millisecondsAndBelow.toFixed(millisecondsDecimalDigits) : Math.ceil(millisecondsAndBelow);
+			const millisecondsString = millisecondsDecimalDigits ? millisecondsAndBelow.toFixed(millisecondsDecimalDigits) : roundedMiliseconds;
 
-			add(parseFloat(millisecondsString, 10), 'millisecond', 'ms', millisecondsString);
+			add(Number.parseFloat(millisecondsString, 10), 'millisecond', 'ms', millisecondsString);
 		}
 	} else {
 		const seconds = (milliseconds / 1000) % 60;
 		const secondsDecimalDigits = typeof options.secondsDecimalDigits === 'number' ? options.secondsDecimalDigits : 1;
-		const secondsFixed = seconds.toFixed(secondsDecimalDigits);
+		const secondsFixed = floorDecimals(seconds, secondsDecimalDigits);
 		const secondsString = options.keepDecimalsOnWholeSeconds ? secondsFixed : secondsFixed.replace(/\.0+$/, '');
-		add(parseFloat(secondsString, 10), 'second', 's', secondsString);
+		add(Number.parseFloat(secondsString, 10), 'second', 's', secondsString);
 	}
 
 	if (result.length === 0) {
@@ -74,12 +99,13 @@ module.exports = (milliseconds, options = {}) => {
 	}
 
 	if (options.compact) {
-		return '~' + result[0];
+		return result[0];
 	}
 
 	if (typeof options.unitCount === 'number') {
-		return '~' + result.slice(0, Math.max(options.unitCount, 1)).join(' ');
+		const separator = options.colonNotation ? '' : ' ';
+		return result.slice(0, Math.max(options.unitCount, 1)).join(separator);
 	}
 
-	return result.join(' ');
+	return options.colonNotation ? result.join('') : result.join(' ');
 };
